30f19e444d912310c87bdc6e2a5a95de32a3b714
Neil Mithipati <neil.mithipati@gmail.com>
2014-06-16 12:08:31 -0700

    Refactor reverseArray method

--- reverseArray.js
+++ reverseArray.js
@@ -1,9 +1,10 @@
 function reverseArray(arr) {
-  var N = arr.length;
-  var temp;
+  var N = arr.length,
+      temp
   for (var i = 0; i < N/2; i++) {
     temp = arr[i];
     arr[i] = arr[N-1-i];
     arr[N-1-i] = temp;
   }
+  return arr;
 }
