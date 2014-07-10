f3190e35190ec48c526588c13813799f10acc6bf
Neil Mithipati <neil.mithipati@gmail.com>
2014-07-10 10:52:20 -0700

    Refactor binary search

--- binarySearch.js
+++ binarySearch.js
@@ -3,7 +3,8 @@ function binarySearch(key, arr) {
   var hi = arr.length - 1;
   var mid;
 
-  while (lo <= hi) {
+  while (lo <= hi)
+  {
     mid = lo + (hi - lo)/2;
     if      (key < arr[mid]) hi = mid - 1;
     else if (arr[mid] < key) lo = mid + 1;
